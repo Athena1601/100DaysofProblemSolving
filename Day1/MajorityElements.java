5eb25f92d9cbca021d2a0e4ba7e0cca00fb3be4d
Athena1601 <95461317+Athena1601@users.noreply.github.com>
2022-11-01 19:52:56 +0530

    Update MajorityElements.java

--- Day1/MajorityElements.java
+++ Day1/MajorityElements.java
@@ -1,1 +1,11 @@
 
+class Solution {
+    public int majorityElement(int[] nums) {
+        // int count = int(nums.length/2);
+        
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+        
+        
+    }
+}
