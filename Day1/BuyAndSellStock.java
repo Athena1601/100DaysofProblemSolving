56ddb8d0b69b64fc603b1a6b9b4eea709ca703b0
Athena1601 <95461317+Athena1601@users.noreply.github.com>
2022-11-01 19:45:32 +0530

    Update BuyAndSellStock.java

--- Day1/BuyAndSellStock.java
+++ Day1/BuyAndSellStock.java
@@ -1,1 +1,26 @@
 
+class Solution {
+    public int maxProfit(int[] prices) {
+        //corner case
+        if(prices.length==0)
+        {
+            return 0;
+        }
+        
+        int max = 0;
+        int minsofar = prices[0];
+        
+        for(int i=0;i<prices.length;i++)
+        {
+            if(prices[i]>minsofar)
+            {
+                max = Math.max(max,prices[i]-minsofar);
+            }
+            else
+            {
+                minsofar=prices[i];
+            }
+        }
+        return max;
+    }
+}
